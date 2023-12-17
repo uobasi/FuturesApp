2b4fe9b1cf31408878dfadb981128d87c7fdf99d
uobasi <54387747+uobasi@users.noreply.github.com>
2023-12-17 17:35:47 -0500

    .

--- app.py
+++ app.py
@@ -890,7 +890,7 @@ app.layout = html.Div([
 
     html.Div(dcc.Input(id='input-on-submit', type='text')),
     html.Button('Submit', id='submit-val', n_clicks=0),
-    html.Div(id='container-button-basic',children="Enter a symbol from |ESH4 NQH4 CLG4 GCG4 NGG4 HGH4 ZNH4 ZTH4 YMH4 6EH4| and submit"),
+    html.Div(id='container-button-basic',children="Enter a symbol from |'ESH4' 'NQH4' 'CLG4' 'GCG4' 'NGG4' 'HGH4' 'ZNH4' 'ZTH4' 'YMH4' '6EH4'| and submit"),
     dcc.Store(id='stkName-value')
 ])
 
