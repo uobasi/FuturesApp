0b16ddc1910cdb30842764c501c9d3fa953fdb91
uobasi <54387747+uobasi@users.noreply.github.com>
2024-02-27 15:19:26 -0500

    .

--- app.py
+++ app.py
@@ -893,22 +893,20 @@ def plotChart(df, lst2, num1, num2, x_fake, df_dx, optionOrderList, stockName=''
     for trds in sortadlist:
         try:
             if str(trds[3]) == 'A':
-                vallue = 'Sell'
+                vallue = 'sell'
                 sidev = trds[0]
             elif str(trds[3]) == 'B':
-                vallue = 'BUY'
+                vallue = 'buy'
                 sidev = trds[0]
             else:
                 vallue = 'Mid'
                 sidev = df['open'][trds[7]]
             fig.add_annotation(x=df['time'][trds[7]], y=sidev,
-                               text= str(trds[4]) + ' ' + str(trds[1]), #+ ' ' + vallue ,
+                               text= str(trds[4]) + ' (' + str(trds[1]) + ') ' + vallue ,
                                showarrow=True,
                                arrowhead=4,
                                font=dict(
-                #family="Courier New, monospace",
                 size=10,
-                # color="#ffffff"
             ),)
         except(KeyError):
             continue 
