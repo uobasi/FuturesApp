5e01bc7583e556cf2445aeec55df23782d49b3ca
uobasi <54387747+uobasi@users.noreply.github.com>
2024-03-11 15:29:20 -0400

    .

--- app.py
+++ app.py
@@ -1082,7 +1082,7 @@ bucket = gclient.get_bucket("stockapp-storage")
 
 import pandas_ta as ta
 from dash import Dash, dcc, html, Input, Output, callback, State
-inter = 80000
+inter = 80001
 app = Dash()
 app.layout = html.Div([
     
