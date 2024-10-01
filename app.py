f46353df322463fe6b827cce60e9b06a7911833b
245 <54387747+uobasi@users.noreply.github.com>
2024-10-01 02:45:06 -0400

    .

--- app.py
+++ app.py
@@ -1911,9 +1911,9 @@ def update_graph_live(n_intervals, sname, interv, stored_data, previous_stkName,
             'Time': 'first'   # Keep the first time for the resampled period
         })
 
-        df['LowVA'] = dfVA['LowVA']
-        df['HighVA'] = dfVA['HighVA']
-        df['POC']  = dfVA['POC']
+        df['LowVA'] = resampled_dfVA['LowVA']
+        df['HighVA'] = resampled_dfVA['HighVA']
+        df['POC']  = resampled_dfVA['POC']
 
 
 
